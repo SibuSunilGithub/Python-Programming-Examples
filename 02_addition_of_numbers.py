762201015f3e956e182d454d944f6c70d99501c7
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-10-27 23:50:24 +0530

    Add Two Number Program
    Python program to add two numbers given by users...

---
+++ 02_addition_of_numbers.py
@@ -0,0 +1,14 @@
+# Python program to add two numbers given by users...
+
+# simple addition of 5 & 2 
+
+num1=5
+num2=2
+print ("Addition of numbers is:",num1+num2)
+
+# number given bgy users...
+
+number1=input('Enter first number: ')
+number2=input('Enter second number: ')
+sum=float(number1)+float(number2)
+print('The sum of two number is: ',sum)
