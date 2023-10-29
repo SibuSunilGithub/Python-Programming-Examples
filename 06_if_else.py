c0768c5297d01b66318092f5c50e7e13acb0bb9b
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-10-29 21:50:42 +0530

    If Else Statements In Python
    Demonstration of if...else statements in python programs..

---
+++ 06_if_else.py
@@ -0,0 +1,8 @@
+# Demonstration of if statements in python programs...
+
+a=int(input("Enter your age: "))
+print("Your age is: ",a)
+if(a>18):
+    print("You can drive.")
+else:
+   print ("You can not drive.")
