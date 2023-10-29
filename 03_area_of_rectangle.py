ccacd012f2dd725ceb147e3cadca1baeb37be09a
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-10-29 21:41:46 +0530

    Area Of Rectangle
    WAP In Python To Find Area Of The Rectangle.

---
+++ 03_area_of_rectangle.py
@@ -0,0 +1,6 @@
+# Find area of rectangle...
+# area= width*height
+
+width= input("Enter the width of the rectangle:")
+height= input("Enter the height of the rectangle:")
+print("Area of the rectangle is:",float(width)*float(height))
