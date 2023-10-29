c94bae4d49ae1ba67df7b7ddc62ed27c4996de37
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-10-29 21:47:26 +0530

    Arithmetic Operations
    WAP In Python And Use All Arithmetic Operators.

---
+++ 05_arithmetic_operations.py
@@ -0,0 +1,11 @@
+# Python programs to performs basic arithmetic operations...
+
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+print("Addition is: ",num1+num2)
+print("Substraction is: ",num1-num2)
+print("Multiplication is: ",num1*num2)
+print("Exponent is: ",num1**num2)
+print("Division is: ",num1/num2)
+print("Modulus is: ",num1%num2)
+print("Floor Division is: ",num1//num2)
