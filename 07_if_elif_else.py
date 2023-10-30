8a6dc7fe4cf085b6f51d79556f3b9be1f5e2e1af
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-10-30 22:53:56 +0530

    if-elif-else statements
    Demonstration of if-elif-else statements in Python programs.

---
+++ 07_if_elif_else.py
@@ -0,0 +1,12 @@
+# Demonstration of if-elif-else statements in python programs...
+
+num=int(input("Enter the number:"))
+if(num<0):
+    print("Number is negative.")
+elif(num==0):
+    ("Number is zero.")
+elif(num==999):
+    ("Number is special.")
+else:
+    print("Number is positive.")
+print("I am happy!!!")
